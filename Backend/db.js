881199c1eea20788c478162cc8778ec2f429e041
skrijwan <skrijwan329@gmail.com>
2024-11-25 23:21:49 +0530

    update

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 // const mongoURI=process.env.mongoURI
 // const mongoURI='mongodb://localhost:27017/musicappDB'
-const mongoURI='mongodb+srv://rijwansk329:SKRIJWAN%402006@clusterdb.7cmvf.mongodb.net/'
+const mongoURI='mongodb+srv://rijwansk329:SKRIJWAN%402006@clusterdb.7cmvf.mongodb.net/musicappDB'
 // const mongoURI=process.env.mongoURI
 
 connectserver=async()=>{
